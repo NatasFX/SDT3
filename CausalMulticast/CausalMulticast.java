86bfef5abba41f9c31e02dea10226d2a91662599
NatasFX <nischmitt@inf.ufsm.br>
2023-06-30 11:11:43 -0300

    Adicionado JavaDoc

--- CausalMulticast/CausalMulticast.java
+++ CausalMulticast/CausalMulticast.java
@@ -10,6 +10,9 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
+/**
+ * Classe principal.
+ */
 public class CausalMulticast {
 
     private Map<Integer, ArrayList<Integer>> vectorClock = new HashMap<>(); // Relógio vetorial
@@ -25,11 +28,14 @@ public class CausalMulticast {
 
     private Thread thread;
 
-    private int QNT_CLIENTES = 3;
+    private int QNT_CLIENTES = 2;
 
     Scanner scanf;
 
-    // inicializa tudo com -1, exceto o que representa esse processo que inicia com 0
+    /**
+    * inicializa tudo com -1, exceto o que representa esse processo que inicia com 0.
+    * @param name Inteiro que representa o cliente
+    */
     private void createVectorClock(Integer name) {
         vectorClock.put(name, new ArrayList<Integer>());
         for (int i = 0; i < QNT_CLIENTES; i++) {
@@ -40,6 +46,12 @@ public class CausalMulticast {
         }
     }
 
+    /**
+    * Construtor principal da classe.
+    * @param ip Inteiro que representa o cliente
+    * @param port Inteiro que representa a porta utilizada para comunicação
+    * @param client Referência para o cliente
+    */
     public CausalMulticast(String ip, Integer port, ICausalMulticast client) {
         // Inicialização do middleware
         this.client = client;
@@ -75,6 +87,9 @@ public class CausalMulticast {
         thread.start();
     }
 
+    /**
+    * Encontra os outros clientes que estão conectados no mesmo canal multicast.
+    */
     private void findOtherClients() throws Exception {
         print("Detectando outras máquinas...");
 
@@ -110,15 +125,27 @@ public class CausalMulticast {
         print("Computadores conectados no grupo: " +  members.toString());
     }
 
-    // ninguem gosta de system.out.meudeus.quanto.negocio.eu.so.quero.printar
+    /**
+    * Função auxiliar para printar bonito no terminal.
+    */
     private void print(String m) {
         System.out.println("[MIDDLEWARE] " + m);
     }
 
-    private String encode(String destinatario, String msg) {
+    /**
+    * Função que codifica dados para uma string que possa ser enviada para outro cliente,
+    * de forma que seja possível extrair de volta as informações originais.
+    * @param destinatario Inteiro que representa o nome do destinatario
+    * @param msg String contendo a mensagem a ser enviada
+    */
+    private String encode(Integer destinatario, String msg) {
         return name + ":" + destinatario + ":" + msg + ":" + vectorClock.get(name).toString();
     }
 
+    /**
+    * Função que define a mensagem é destinada ao cliente atual.
+    * @param msg String contendo a mensagem para análise
+    */
     private boolean message_for_me(String msg) {
         if (!msg.contains(":")) return false;
         
@@ -126,6 +153,10 @@ public class CausalMulticast {
         return Integer.decode(data[1]) == name;
     }
 
+    /**
+    * Função que envia a mensagem no canal multicast.
+    * @param msg String contendo a mensagem a ser enviada
+    */
     private void send(String msg) {
         DatagramPacket packet = new DatagramPacket(msg.getBytes(), msg.length(), group, port);
         try {
@@ -133,6 +164,11 @@ public class CausalMulticast {
         } catch (Exception e) { e.printStackTrace(); }
     }
 
+    /**
+    * Função exposta ao cliente para ele enviar uma mensagem quando solicitado.
+    * @param msg String contendo a mensagem a ser enviada
+    * @param msg Referência ao cliente
+    */
     public void mcsend(String msg, ICausalMulticast client) {
 
         if (msg.contains(":")) {
@@ -156,7 +192,7 @@ public class CausalMulticast {
         
         for (Integer nome : members) {
             if (nome.equals(name)) continue;
-            String m = encode(nome.toString(), msg);
+            String m = encode(nome, msg);
             
             if (ask("Devo enviar para \"" + nome + "\"?"))
                 send(m);
@@ -164,11 +200,15 @@ public class CausalMulticast {
         }
 
         // Incrementa o relógio vetorial
-        buffer.add(new Message(encode(name.toString(), msg), true));
+        buffer.add(new Message(encode(name, msg), true));
         incrementVectorClock(name);
         print("Meu vetor logico: " + vectorClock.get(name).toString());
     }
     
+    /**
+    * Função auxiliar para o middleware pedir algo ao cliente.
+    * @param m Mensagem a ser exibida no terminal contendo a pergunta a ser respondida com [sim/nao]
+    */
     private boolean ask(String m) {
         while (true) {
             print(m+" [y/n]");
@@ -181,10 +221,19 @@ public class CausalMulticast {
         }
     }
 
+    /**
+    * Função que incrementa o vetor de relógio lógico do processo fornecido.
+    * @param processId Inteiro que representa o processo
+    */
     private void incrementVectorClock(Integer processId) {
         vectorClock.get(name).set(processId, vectorClock.get(name).get(processId) + 1);
     }
 
+    /**
+    * Função auxiliar para converter a representação do vetor lógico em piggyback recebido na mensagem
+    * que está em string, para um ArrayList<Integer> o qual podemos utilizar.
+    * @param s Representação em string do ArrayList<Integer> para conversão
+    */
     private ArrayList<Integer> strToVC(String s) {
         s = s.replaceAll("\\[|\\]", "");
         ArrayList<Integer> temp_vc = new ArrayList<>();
@@ -198,7 +247,11 @@ public class CausalMulticast {
         return temp_vc;
     }
 
-    // atualiza com base no algoritmo para estabilização de mensagens
+    /**
+    * Função que atualiza com base no algoritmo para estabilização de mensagens.
+    * @param sender Inteiro que representa o remetente
+    * @param VC String que foi recebida na mensagem original que representa o vetor em piggyback
+    */
     private void updateVectorClock(Integer sender, String VC) {
         ArrayList<Integer> array = strToVC(VC);
 
@@ -209,7 +262,9 @@ public class CausalMulticast {
         }
     }
 
-    // Verifica se é possível entregar mensagens do buffer de acordo com o relógio vetorial
+    /**
+    * Função que verifica se é possível entregar mensagens do buffer de acordo com o relógio vetorial.
+    */
     private void causalOrder() {
         for (Message msg : buffer) {
             if(!msg.isDelivered()){
@@ -230,7 +285,10 @@ public class CausalMulticast {
         }
     }
 
-    private void stabilization(){
+    /**
+    * Função que descarta mensagens que já foram entregues e estão estabilizadas.
+    */
+    private void estabilização() {
         for (int index = 0; index < buffer.size(); index++) {
             Message msg = buffer.get(index);
             if (msg.isDelivered()) {
@@ -248,6 +306,7 @@ public class CausalMulticast {
 
                     if (vcmsg >= mci_x) {
                         canDiscard = false;
+                        break;
                     }
                 }
 
@@ -260,6 +319,9 @@ public class CausalMulticast {
         }
     }
     
+    /**
+    * Função que ordena o buffer de acordo com a ordem dos vetores de piggyback.
+    */
     private void bufferSort() {
         buffer.sort((msg1, msg2) -> {
             ArrayList<Integer> vc1List = strToVC(msg1.getContent().split(":")[3]);
@@ -276,6 +338,9 @@ public class CausalMulticast {
         });
     }
 
+    /**
+    * Classe para receber as mensagens de forma assíncrona.
+    */
     class Receiver extends Thread {
 
         Integer name;
@@ -283,13 +348,19 @@ public class CausalMulticast {
         private List<Integer> members;
         
         public List<String> messages = new ArrayList<String>();
-
+        
+        /**
+        * Construtor da classe.
+        */
         public Receiver(Integer name, MulticastSocket socket, List<Integer> members) {
             this.name = name;
             this.socket = socket;
             this.members = members;
         }
 
+        /**
+        * Função auxiliar para print.
+        */
         private void print(String m) {
             System.out.println("\r[MIDDLEWARE] " + m);
         }
@@ -310,7 +381,7 @@ public class CausalMulticast {
                 if (!s.contains(":")) { // mensagem inicial
                     if (!members.contains(Integer.decode(s))) {
 
-                        QNT_CLIENTES += 1; // essa parte aqui tá bem rudimentar, precisa ser melhorada
+                        QNT_CLIENTES += 1;
                         
                         try {
                             findOtherClients();
@@ -330,33 +401,50 @@ public class CausalMulticast {
                     bufferSort();
                     
                     causalOrder();
-                    stabilization();
+                    estabilização();
 
                     print("Meu vetor logico agora: " + vectorClock.get(name).toString());
-                    print("Minha matriz: \n" + vectorClock.get(0).toString() + "\n" +
-                    vectorClock.get(1).toString() + "\n" + vectorClock.get(2).toString());
+                    print("Minha matriz:");
+                    for (int i = 0; i < QNT_CLIENTES; i++) {
+                        print(vectorClock.get(i).toString());
+                    }
                 }
             }
         }
     }
 
+    /**
+    * Classe para representação da mensagem e seu estado.
+    */
     class Message {
         private String content;
         private boolean delivered;
 
+        /**
+        * Cosntrutor da classe.
+        */
         public Message(String content, boolean delivered) {
             this.content = content;
             this.delivered = delivered;
         }
 
+        /**
+        * Retorna o conteúdo da mensagem.
+        */
         public String getContent() {
             return content;
         }
 
+        /**
+        * Retorna se a mensagem já foi entregue ou não.
+        */
         public boolean isDelivered() {
             return delivered;
         }
 
+        /**
+        * Indica se a mensagem tem flag entregue ou não.
+        */
         public void setDelivered(boolean delivered) {
             this.delivered = delivered;
         }
